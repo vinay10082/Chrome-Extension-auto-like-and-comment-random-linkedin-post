1b55bf062bac9d9481bc9b3ff7557b225e3fcc46
vinay10082 <20bph043@nith.ac.in>
2022-10-29 18:25:35 +0530

    changes I made

--- popup.js
+++ popup.js
@@ -1,53 +1,35 @@
-const like = document.querySelector('.like-btn');
-const comment = document.querySelector('.comment-btn');
+const btn = document.querySelector('.btn');
 
-like.addEventListener('click', function() {
+btn.addEventListener('click', function() {
     var like_count = document.querySelector('#like-count').value;
-
-    chrome.tabs.query({ active:true, currentWindow: true},(tabs) =>{
-        chrome.scripting.executeScript({
-            target: {tabId: tabs[0].id},
-            func: likeOnPost,
-            args: [like_count]
-        });
-
-    });
-
-});
-
-function likeOnPost(count) {
-
-    var like_count = `${count}`
-
-    console.log(like_count)
-
-    document.querySelector(".artdeco-button__text .react-button__text\n            ").click();
-}
-
-comment.addEventListener('click', function() {
     var comment_count = document.querySelector('#comment-count').value;
-    console.log(comment_count);
+
+
     chrome.tabs.query({ active:true, currentWindow: true},(tabs) =>{
         chrome.scripting.executeScript({
             target: {tabId: tabs[0].id},
-            func: commentOnPost,
-            args: [comment_count]
+            func: likeandCommentOnPost,
+            args: [like_count,comment_count]
         });
 
     });
-    
+
 });
 
+function likeandCommentOnPost(count1,count2) {
 
-function commentOnPost(count) {
+    var like_count = `${count1}`
+    console.log(like_count)
+    document.querySelector(".artdeco-button__text .react-button__text\n            ").click();
 
-    var comment_count = `${count}`
+    setTimeout(() => {
+        
+    }, 10000);
 
-        console.log(comment_count)
-
-            document.querySelector(".ql-editor").innerHTML="<p>I am testing my bot.Please! ignore comment</p>";
-            setTimeout(() => {
-                document.querySelector(".comments-comment-box__submit-button").click();
-            }, 1000);
-
+    var comment_count = `${count2}`
+    console.log(comment_count)
+        document.querySelector(".ql-editor").innerHTML="<p>I am testing my bot.Please! ignore comment</p>";
+        setTimeout(() => {
+            document.querySelector(".comments-comment-box__submit-button").click();
+        }, 1000);
 }
