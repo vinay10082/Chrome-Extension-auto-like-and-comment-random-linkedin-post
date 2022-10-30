c688f685181851f825c9c3ad417b242b2b429790
vinay10082 <20bph043@nith.ac.in>
2022-10-30 18:46:55 +0530

    make it better

--- popup.js
+++ popup.js
@@ -1,11 +1,15 @@
 const btn = document.querySelector('.btn');
 
-btn.addEventListener('click', () => {
-    var like_count = document.querySelector('#like-count').value;
-    var comment_count = document.querySelector('#comment-count').value;
+
+btn.addEventListener('click',function (){
+    
+    var like_count = document.getElementById('like-count').value;
+    console.log(like_count);
+    var comment_count = document.getElementById('comment-count').value;
+    console.log(comment_count);
 
     chrome.tabs.update({url: 'https://linkedin.com/'});
-
+    
     setTimeout(() => {
         chrome.tabs.query({ active:true, currentWindow: true},(tabs) =>{
             chrome.scripting.executeScript({
@@ -15,14 +19,14 @@ btn.addEventListener('click', () => {
            });
         })    
     }, 10000);
-
 })
 
 function likeandCommentOnPost(count1,count2) {
 
     var like_count = `${count1}`
-    console.log(like_count)
-    document.querySelector(".artdeco-button__text .react-button__text\n            ").click();
+
+            console.log(like_count)
+            document.querySelector(".artdeco-button__text .react-button__text\n            ").click();
 
     setTimeout(() => {
         
@@ -30,7 +34,7 @@ function likeandCommentOnPost(count1,count2) {
 
     var comment_count = `${count2}`
     console.log(comment_count)
-        document.querySelector(".ql-editor").innerHTML="<p>I am testing my bot.Please! ignore comment</p>";
+        document.querySelector(".ql-editor").innerHTML="<p>CFBR</p>";
         setTimeout(() => {
             document.querySelector(".comments-comment-box__submit-button").click();
         }, 1000);
